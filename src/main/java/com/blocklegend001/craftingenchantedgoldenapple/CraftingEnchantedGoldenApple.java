4a1129b80d9482b3c623d1d81659e29c4d9545b7
BlockCreator001 <62543902+BlockCreator001@users.noreply.github.com>
2022-07-09 17:51:11 +0200

    Add files via upload

--- src/main/java/com/blocklegend001/craftingenchantedgoldenapple/CraftingEnchantedGoldenApple.java
+++ src/main/java/com/blocklegend001/craftingenchantedgoldenapple/CraftingEnchantedGoldenApple.java
@@ -1,5 +1,6 @@
 package com.blocklegend001.craftingenchantedgoldenapple;
 
+import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -13,31 +14,26 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CraftingEnchantedGoldenApple.MOD_ID)
 public class CraftingEnchantedGoldenApple {
-
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     public static final String MOD_ID = "craftingenchantedgoldenapple";
 
     public CraftingEnchantedGoldenApple() {
-        // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         eventBus.addListener(this::setup);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
